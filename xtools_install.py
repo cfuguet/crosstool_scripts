2c62b9d368e8787629a79806141090ab7ca502bc
Cesar Fuguet <cesar.fuguet@protonmail.com>
2024-10-21 07:00:40 +0200

    Add GPL license

--- xtools_install.py
+++ xtools_install.py
@@ -1,28 +1,23 @@
 #!/usr/bin/env python3
-##
-# MIT License
+## 
+#  Cross-toolchain build script
+#  Copyright (C) 2024 Cesar Fuguet
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-##
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ##
 # @file   xtools_install.py
-# @author Cesar Fuguet Tortolero
+# @author Cesar Fuguet
 ##
 
 """This script installs a cross-toolchain for a given target architecture
@@ -65,8 +60,8 @@ CONFIG = {
     'nparallel' : 8,
 
     #  extra configure options
-    'gcc_configure_extra_options' :      '--with-isa-spec=2.2',
-    'binutils_configure_extra_options' : '--with-isa-spec=2.2',
+    # 'gcc_configure_extra_options' :      '--with-isa-spec=2.2',
+    # 'binutils_configure_extra_options' : '--with-isa-spec=2.2',
 
     #  base directories shared by all tools
     'archive_dir' :                      os.path.join(SCRIPT_DIR, 'archives'),
